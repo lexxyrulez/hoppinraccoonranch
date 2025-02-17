32b109aee0ec6359be1b4c4f5aadbf64ac72fe82
lexxyrulez <alexchambika85@gmail.com>
2025-02-17 21:27:43 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Ensure correct alias
+      "@/": path.resolve(__dirname, "./src"),
     },
   },
   base: "/hoppinraccoonranch/", // Set correct base for GitHub Pages

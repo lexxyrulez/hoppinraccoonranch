5b8970485e46e9df9c7cde13230b9309a027356a
lexxyrulez <alexchambika85@gmail.com>
2025-02-17 21:31:05 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@/": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: "/hoppinraccoonranch/", // Set correct base for GitHub Pages
-  root: path.resolve(__dirname, "client"), // Set Vite's root directory
-  build: {
-    outDir: path.resolve(__dirname, "dist"), // Match with gh-pages
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000", // Proxy backend API requests
-    },
-  },
-});

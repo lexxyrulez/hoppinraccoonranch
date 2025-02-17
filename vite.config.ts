00fdd30d4c785470549741f8829af15bec870ca3
lexxyrulez <alexchambika85@gmail.com>
2025-02-17 21:19:49 +0300

    vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import checker from "vite-plugin-checker";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({ typescript: true, overlay: false }),
-    runtimeErrorOverlay(),
-  ],
- 
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // This assumes you want to use @ as an alias for the src directory
+      "@": path.resolve(__dirname, "client", "src"), // Make sure "client/src" is correct
     },
   },
-
-
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "build"), // Using "build" as output directory
+    outDir: path.resolve(__dirname, "build"),
     emptyOutDir: true,
   },
 });

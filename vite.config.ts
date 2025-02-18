b8285e1096dfec45cbb390af1d4b7efd75ffec1f
lexxyrulez <alexchambika85@gmail.com>
2025-02-18 15:45:18 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
 import checker from "vite-plugin-checker";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal"
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [
     react(),
     checker({ typescript: true, overlay: false }),
     runtimeErrorOverlay(),
-    // themePlugin(),
-,
+    // themePlugin(), // Uncomment this if you are using @replit/vite-plugin-shadcn-theme-json
   ],
   resolve: {
     alias: {
